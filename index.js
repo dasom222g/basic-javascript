9a23f2c53a4db9f9ab2b748c52cea9af6c80ad9a
dasom222g <dasom228@gmail.com>
2025-04-08 12:01:13 +0900

    feat: 조건문

--- index.js
+++ index.js
@@ -40,17 +40,64 @@ greeting("후츠릿"); // "후츠릿": 인자(argument)
 // 미션
 // add라는 함수를 만들어서 2개의 숫자를 받아와서 두 숫자의 합을 console에 찍는 함수를 만들어보세요
 
-function add(num1, num2) {
+const add = (num1, num2) => {
   result = num1 + num2;
   // console.log("두 숫자의 합은", result, "입니다");
   console.log(`두 숫자의 합은 ${result}입니다`);
   return result;
-}
+};
 
-function double(num) {
-  return num * 2;
-}
+// function double(num) {
+//   return num * 2;
+//   console.log("🔥🔥함수 종료!!");
+// }
+
+// const double = (num) => {
+//   return num * 2;
+// };
+
+const double = (num) => num * 2;
 
 const sum = add(12, 345);
 const doubleResult = double(sum);
 console.log("🚀 ~ doubleResult:", doubleResult);
+
+function validCheck(text) {
+  // 유효성 체크
+  const check = text !== ""; // boolean
+  if (check === false) {
+    return;
+  }
+
+  console.log("유효한 값입니다");
+}
+
+validCheck("후츠릿");
+
+console.log("==================");
+
+// 조건문
+const score = 50;
+
+// if (score >= 60) {
+//   console.log("합격");
+// } else {
+//   console.log("불합격");
+// }
+
+// score >= 60 ? console.log("합격") : console.log("불합격");
+
+console.log(score >= 60 ? "합격" : "불합격");
+
+// if (score >= 90) {
+//   console.log("A등급");
+// } else if (score >= 80) {
+//   console.log("B등급");
+// } else if (score >= 70) {
+//   console.log("C등급");
+// } else {
+//   // 70점 미만인경우
+//   console.log("D등급");
+// }
+
+console.log("끝!!!");
